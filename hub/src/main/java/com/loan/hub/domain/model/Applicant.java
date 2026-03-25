a181ca7f37eda9bd60e7ed5d8c6309bb8b2c4062
Karuna Kumar <karuna.anakarla@example.com>
2026-03-25 17:33:36 +0530

    refine models and configuration for validation and logging

--- hub/src/main/java/com/loan/hub/domain/model/Applicant.java
+++ hub/src/main/java/com/loan/hub/domain/model/Applicant.java
@@ -3,18 +3,22 @@ package com.loan.hub.domain.model;
 import java.math.BigDecimal;
 
 import com.loan.hub.domain.enums.EmploymentType;
-import jakarta.validation.constraints.*;;
+import jakarta.validation.constraints.*;
+import lombok.Getter;
+import lombok.Setter;;
 /**
  * Represents applicant details for loan processing.
  */
+@Setter
+@Getter
 public class Applicant {
-    
+
     @NotBlank(message = "Name is required")
     private String name;
 
     @Min(value = 21, message = "Age must be at least 21")
     @Max(value = 60, message = "Age must not exceed 60")
-    private int age;
+    private Integer age;
 
     @NotNull(message = "Monthly income is required")
     @DecimalMin(value = "0.0", inclusive = false, message = "Income must be greater than 0")
@@ -25,6 +29,6 @@ public class Applicant {
 
     @Min(value = 300, message = "Credit score must be >= 300")
     @Max(value = 900, message = "Credit score must be <= 900")
-    private int creditScore;
+    private Integer creditScore;
 
 }
